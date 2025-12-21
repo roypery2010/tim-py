3c71510557b3f75a9f6047dfcb98c7c204c9a4c7
roypery2010 <roypery2010@gmail.com>
2025-12-21 10:00:02 +0200

    Added NOP And HALT instructions

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ import pickle
 
 class Inst_Set(Enum):
     INST_PUSH = 0
-    INST_POP = 1
-    INST_DUP = 2
-    INST_SWAP = 3
-    INST_ADD = 4
-    INST_SUB = 5
-    INST_MUL = 6
-    INST_DIV = 7
-    INST_CMPE = 8
-    INST_CMPNE = 9
-    INST_CMPG = 10
-    INST_CMPL = 11
-    INST_CJMP = 12
-    INST_JMP  = 13
-    INST_PRINT = 14
+    INST_NOP = 1
+    INST_HALT = 2
+    INST_POP = 3
+    INST_DUP = 4
+    INST_SWAP = 5
+    INST_ADD = 6
+    INST_SUB = 7
+    INST_MUL = 8
+    INST_DIV = 9
+    INST_MOD = 10
+    INST_CMPE = 11
+    INST_CMPNE = 12
+    INST_CMPG = 13
+    INST_CMPL = 14
+    INST_CMPGE = 15
+    INST_CMPLE = 16
+    INST_CJMP = 17
+    INST_JMP  = 18
+    INST_PRINT = 19
 
 class Inst:
     def __init__(self, inst_type, val=0):
@@ -25,6 +30,10 @@ class Inst:
 
 def DEF_INST_PUSH(x):
     return Inst(Inst_Set.INST_PUSH, x)
+def DEF_INST_NOP():
+    return Inst(Inst_Set.INST_NOP)
+def DEF_INST_HALT():
+    return Inst(Inst_Set.INST_HALT)
 def DEF_INST_POP():
     return Inst(Inst_Set.INST_POP)
 def DEF_INST_DUP():
@@ -39,6 +48,8 @@ def DEF_INST_MUL():
     return Inst(Inst_Set.INST_MUL)
 def DEF_INST_DIV():
     return Inst(Inst_Set.INST_DIV)
+def DEF_INST_MOD():
+    return Inst(Inst_Set.INST_MOD)
 def DEF_INST_CMPE():
     return Inst(Inst_Set.INST_CMPE)
 def DEF_INST_CMPNE():
@@ -47,6 +58,10 @@ def DEF_INST_CMPG():
     return Inst(Inst_Set.INST_CMPG)
 def DEF_INST_CMPL():
     return Inst(Inst_Set.INST_CMPL)
+def DEF_INST_CMPGE():
+    return Inst(Inst_Set.INST_CMPGE)
+def DEF_INST_CMPLE():
+    return Inst(Inst_Set.INST_CMPLE)
 def DEF_INST_JMP(x):
     return Inst(Inst_Set.INST_JMP, x)
 def DEF_INST_CJMP(x):
@@ -75,6 +90,8 @@ class Machine:
             match inst.inst_type:
                 case Inst_Set.INST_PUSH:
                     self.push(inst.val)
+                case Inst_Set.INST_NOP:
+                    continue
                 case Inst_Set.INST_POP:
                     self.self.pop()
                 case Inst_Set.INST_DUP:
@@ -104,6 +121,10 @@ class Machine:
                     if (b == 0):
                         raise RuntimeError("ERROR: Cannot divide by zero\n")
                     self.push(a // b)
+                case Inst_Set.INST_DIV:
+                    a = self.pop()
+                    b = self.pop()
+                    self.push(a % b)
                 case Inst_Set.INST_CMPE:
                     a = self.pop()
                     b = self.pop()
@@ -140,6 +161,24 @@ class Machine:
                         self.push(1)
                     else:
                         self.push(0)
+                case Inst_Set.INST_CMPGE:
+                    a = self.pop()
+                    b = self.pop()
+                    self.push(a)
+                    self.push(b)
+                    if (a >= b):
+                        self.push(1)
+                    else:
+                        self.push(0)
+                case Inst_Set.INST_CMPLE:
+                    a = self.pop()
+                    b = self.pop()
+                    self.push(a)
+                    self.push(b)
+                    if (a <= b):
+                        self.push(1)
+                    else:
+                        self.push(0)
                 case Inst_Set.INST_CJMP:
                     a = self.pop()
                     if (a == 1):
@@ -158,16 +197,19 @@ class Machine:
                 case Inst_Set.INST_PRINT:
                     a = self.pop()
                     print(a)
+                case Inst_Set.INST_HALT:
+                    break
             print(self.stack)
 
 program = [
-    DEF_INST_PUSH(1),
-    DEF_INST_PUSH(2),
-    DEF_INST_CMPE(),
-    DEF_INST_CJMP(10),
-    DEF_INST_PUSH(2),
-    DEF_INST_ADD(),
-    DEF_INST_PUSH(4),
+    DEF_INST_PUSH(14),
+    DEF_INST_PUSH(27),
+    DEF_INST_HALT(),
+    DEF_INST_PUSH(15),
+    DEF_INST_CMPGE(),
+    DEF_INST_NOP(),
+    DEF_INST_NOP(),
+    DEF_INST_NOP(),
     DEF_INST_PRINT(),
 ] 
 PROGRAM_SIZE = len(program)
